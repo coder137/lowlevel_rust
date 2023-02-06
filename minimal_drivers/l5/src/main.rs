a1be7b1aba8634841ffba37179b35194eb739ffb
Niket Naidu <coder137@gmail.com>
2023-02-06 00:30:05 -0800

    Added l5 with updated main example

--- minimal_drivers/l5/src/main.rs
+++ minimal_drivers/l5/src/main.rs
@@ -1,19 +1,40 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 
-mod blink;
+use l3::*;
+use l4::*;
 
 #[cfg(not(test))]
+#[cfg(all(target_arch = "arm", target_os = "none"))]
+pub fn spin_delay(delay: u32) {
+    use core::arch::asm;
+
+    let mut mdelay = delay;
+    while mdelay != 0 {
+        unsafe {
+            asm!("nop");
+        }
+        mdelay -= 1;
+    }
+}
+
+#[cfg(not(test))]
+#[cfg(all(target_arch = "arm", target_os = "none"))]
 #[no_mangle]
 fn main() -> ! {
     const TIME: u32 = 100_000;
-    blink::blink_init();
-    blink::blink_set();
+
+    // TODO, This should go instead GPIO
+    let mut rcc = RCC::new(RCC_Port::get());
+    rcc.set_ahb2enr(RCC_AHB2ENR::GPIOAEN);
+
+    let mut gpio = GPIO::configure_as_output(GPIOA_Port::get(), GpioPin::Num5);
+    let mut led = Led::new(&mut gpio);
     loop {
-        blink::_spin_delay(TIME);
-        blink::blink_reset();
-        blink::_spin_delay(TIME);
-        blink::blink_set();
+        led.on();
+        spin_delay(TIME);
+        led.off();
+        spin_delay(TIME);
     }
 }
 
