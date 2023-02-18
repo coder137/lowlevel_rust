9783280215d831bdb8487423cd9cc76aabb815f7
Niket Naidu <coder137@gmail.com>
2023-02-18 06:15:51 -0800

    Updated l0 library

---
+++ minimal_drivers/l0/src/stm32l475xx/private.rs
@@ -0,0 +1,30 @@
+use crate::{get_port, global::SYSTEM_CLOCK, read_register};
+use core::ptr::read_volatile;
+use core::sync::atomic::Ordering;
+
+use crate::controller::{RCC_TypeDef, RCC_BASE};
+
+pub fn controller_init() {
+    // Update the System clock
+    let rcc_port = get_port!(RCC_TypeDef, RCC_BASE);
+    let cr_data = read_register!(rcc_port, CR);
+    let msi_range = (cr_data >> 4) & 0xF;
+    let system_clock: u32 = match msi_range {
+        0 => todo!(),
+        1 => todo!(),
+        2 => todo!(),
+        3 => todo!(),
+        4 => 1_000_000,
+        5 => 2_000_000,
+        6 => 4_000_000,
+        7 => todo!(),
+        8 => todo!(),
+        9 => todo!(),
+        10 => todo!(),
+        11 => todo!(),
+        _ => unreachable!(),
+    };
+    SYSTEM_CLOCK.store(system_clock, Ordering::SeqCst)
+
+    // TODO, Do more things
+}
---
+++ minimal_drivers/l0/src/utility/mod.rs
@@ -0,0 +1,21 @@
+#[macro_export]
+macro_rules! get_port {
+    ($register_map_struct:ident, $address:ident) => {
+        unsafe { &mut *($address as *mut $register_map_struct) }
+    };
+}
+
+#[macro_export]
+macro_rules! read_register {
+    ($port:ident, $register_name:ident) => {
+        unsafe { read_volatile(&$port.$register_name) }
+    };
+}
+
+// TODO, Overload this macro to support $data:literal
+#[macro_export]
+macro_rules! write_register {
+    ($port:ident, $register_name:ident, $data:ident) => {
+        unsafe { write_volatile(&mut $port.$register_name, $data) }
+    };
+}
