84abe5e8bd9b69b29c9abccd6194c22f55fcf732
Niket Naidu <coder137@gmail.com>
2023-04-06 01:39:27 -0700

    Minor update to button lib

--- minimal_async_basic/l4/src/button.rs
+++ minimal_async_basic/l4/src/button.rs
@@ -1,12 +1,12 @@
 use l3::{GpioIn, GpioValue};
 
 pub struct Button<'a> {
-    gpio: &'a mut dyn GpioIn,
+    gpio: &'a dyn GpioIn,
     default: GpioValue, // Default value when button is not pressed
 }
 
 impl<'a> Button<'a> {
-    pub fn new(gpio: &'a mut dyn GpioIn, default: GpioValue) -> Self {
+    pub fn new(gpio: &'a dyn GpioIn, default: GpioValue) -> Self {
         Self { gpio, default }
     }
 
